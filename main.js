e91897ebb32e6bd30e01e7f726362ea4b9f0edb7
Anton Maryanskiy <Maryanskiy44@yandex.ru>
2024-01-03 00:00:56 +0400

    start

---
+++ main.js
@@ -0,0 +1,9 @@
+function changeBlackOutVisible() {
+  console.dir(logo__left);
+  // blackout.style.opacity = 0;
+
+  logo__left.style.opacity = 1;
+  logo__left.style.transform = "translateX(20px) scale(1)";
+}
+
+changeBlackOutVisible();
