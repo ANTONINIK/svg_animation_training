8026b1fe981b074bb55a587197d731a6cfcac00e
Anton Maryanskiy <Maryanskiy44@yandex.ru>
2024-01-03 02:46:13 +0400

    in progress

--- main.js
+++
@@ -1,9 +0,0 @@
-function changeBlackOutVisible() {
-  console.dir(logo__left);
-  // blackout.style.opacity = 0;
-
-  logo__left.style.opacity = 1;
-  logo__left.style.transform = "translateX(20px) scale(1)";
-}
-
-changeBlackOutVisible();
